c8513b376f9cbab750755a17bfd577ebef49cf59
Halton Bagley <haltonba94@gmail.com>
2019-12-13 23:57:03 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,6 +12,6 @@ def input_to_index(input)
   input.to_i - 1
 end
 
-def move(board, input_to_index, character = "X")
-  board[input_to_index] = character
+def move(board, input_to_index, char = "X")
+  board[input_to_index] = char
 end
